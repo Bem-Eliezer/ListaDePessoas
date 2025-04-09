1d3b0a5fe70519eeb20c36c9237f223c6eae04ff
Bem-Eliezer <eliezer.santos1010@gmail.com>
2025-04-09 20:36:31 -0300

    Primeiro Commit

---
+++ SeparadorPorSexo.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SeparadorPorSexo {
+    public static void main(String[] args) {
+        ArrayList<Pessoa> pessoas = new ArrayList<>();
+
+        pessoas.add(new Pessoa("Ana", 28, "F"));
+        pessoas.add(new Pessoa("Carlos", 34, "M"));
+        pessoas.add(new Pessoa("Beatriz", 22, "F"));
+        pessoas.add(new Pessoa("Daniel", 30, "M"));
+
+
+        String sexoDesejado = "F"; 
+          List<Pessoa> grupoSelecionado = pessoas.stream()
+            .filter(p -> {
+                if (p.getSexo().equalsIgnoreCase(sexoDesejado)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+            .collect(Collectors.toList());
+
+        System.out.println("Pessoas do sexo " + sexoDesejado + ":");
+        grupoSelecionado.forEach(System.out::println);
+    }
+}
